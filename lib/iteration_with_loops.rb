2ea7922d95cf3cabba3b640d750418e74dfac6fe
Adao Natalino <sansonov@gmail.com>
2020-03-30 22:57:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,11 @@ def find_even_values(src)
   while x < src.length do
     y = 0
     while y < src[x].length do
-        
+      if src[x][y]%2 === 0
+        puts src[x][y]
+        y+=1
     end
+    x+=1
   end
 
 end
